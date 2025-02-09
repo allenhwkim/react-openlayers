6d7446727b62c91eaa48113ed72fad7770370f1b
Kim, Allen <allen.kim@ontariohealth.ca>
2025-02-09 13:39:18 -0500

    Reorganize files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,14 +6,6 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
---
+++ src/stories/Map.stories.tsx
@@ -0,0 +1,36 @@
+import * as ol from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import { OSM } from 'ol/source';
+import OlMap from '../lib/Map';
+import { useRef } from 'react';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: 'Map',
+  component: OlMap,
+  tags: ['autodocs'],
+  argTypes: {}, // https://storybook.js.org/docs/api/argtypes
+  parameters: {
+  },
+}
+
+const MapWithButton = () => {
+  const map = useRef<ol.Map>();
+ 
+  const showMap = () => {
+    const view = new ol.View({ center: [0, 0], zoom: 2 });
+    map.current.addLayer(new TileLayer({ source: new OSM() }));
+    map.current.setView(view);
+    console.info('react-openlayers: map instance', map.current);
+  };
+
+  return <div>
+    <OlMap ref={map} />
+    <button onClick={showMap}>Show Map</button>
+  </div>
+};
+ 
+export const Primary = {
+  args: {}, // https://storybook.js.org/docs/writing-stories/args
+  render: () => <MapWithButton />
+}
