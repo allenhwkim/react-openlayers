b87b9fdf61e924b7e8ec9c5d5757db8dece1497e
Kim, Allen <allen.kim@ontariohealth.ca>
2025-02-15 22:56:46 -0500

    Update config for React lib build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.ts'),
+      formats: ['es']
+    }
+  }
 })
