6ee47499de47b5d3c6e7ae57d775a885d547bed4
Kim, Allen <allen.kim@ontariohealth.ca>
2025-02-17 14:50:05 -0500

    Enable marker to move around using <PointerInteraction />

--- src/lib/interactions/PointerInteraction.tsx
+++ src/lib/interactions/PointerInteraction.tsx
@@ -1,12 +1,46 @@
 import OlPointerInteraction from 'ol/interaction/Pointer';
 import { useEffect } from 'react';
+import { Coordinate } from 'ol/coordinate';
+import { FeatureLike } from 'ol/Feature';
+import MapBrowserEvent from 'ol/MapBrowserEvent';
+import { Geometry } from 'ol/geom'
 import { useMap } from '../Map';
 
 export function PointerInteraction(props) {
   const map = useMap();
 
+  let coordinate: Coordinate;
+  let feature: FeatureLike;
+
+  function handleDownEvent(evt: MapBrowserEvent<UIEvent>) {
+    const _feature = evt.map.forEachFeatureAtPixel(evt.pixel, feature => feature);
+    _feature && ([coordinate, feature] = [evt.coordinate, _feature]);
+    return !!feature; // `true` to start the drag sequence.
+  }
+
+  function handleDragEvent(evt: MapBrowserEvent<UIEvent>) {
+    const geometry = feature.getGeometry() as Geometry;
+    const deltaX = evt.coordinate[0] - coordinate[0];
+    const deltaY = evt.coordinate[1] - coordinate[1];
+    geometry.translate(deltaX, deltaY);
+    coordinate = evt.coordinate;
+  }
+
+  function handleMoveEvent(evt: MapBrowserEvent<UIEvent>) {
+    const feature = evt.map.forEachFeatureAtPixel(evt.pixel, feature => feature);
+    const element = evt.map.getTargetElement();
+    element.style.cursor = feature ? 'pointer' : '';
+  }
+
+  function handleUpEvent() {
+    coordinate = null;
+    feature = null;
+    return false; // return `false` to stop the drag sequence
+  }
+
   useEffect(() => {
     if (!map) return;
+    props = {...{handleDownEvent, handleMoveEvent, handleUpEvent, handleDragEvent}, ...props };
     const interaction = new OlPointerInteraction(props);
     map.addInteraction(interaction);
   }, [map]);
--- src/stories/Marker/Marker.stories.tsx
+++ src/stories/Marker/Marker.stories.tsx
@@ -1,7 +1,7 @@
 import { OSM } from 'ol/source';
 import { useRef } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Map, TileLayer, View, Marker } from '../../lib';
+import { Map, TileLayer, View, Marker, PointerInteraction } from '../../lib';
 
 export default { title: 'Marker' } as Meta;
 declare let window:any;
@@ -15,6 +15,7 @@ export const Primary: StoryObj<any> = {
         <TileLayer source={new OSM()} />
         <View center={[0,0]} zoom={4}/>
         <Marker lonLat={[0,0]} char="A" color="blue" addOnClick={true} removeOnClick={true} />
+        <PointerInteraction /> {/* Enable marker move */}
       </Map>
     );
   }
